630ff9eee0a4d585b41c6513585724c65398ef87
Owen Cruse <ocruse7@gmail.com>
2019-11-09 23:40:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,27 @@
-def using_include(array, element)
-
+def using_include( array, element )
+  if array.include?(element)
+    return true
+  else
+    return FALSE
+  end
 end
 
-def using_sort(array)
-
+def using_sort( array )
+  return array.sort
 end
 
-def using_reverse(array)
-
+def using_reverse( array )
+  return array.reverse
 end
 
-def using_first(array)
-
+def using_first( array )
+  return array.first
 end
 
-def using_last(array)
-
+def using_last( array )
+  return array.last
 end
 
-def using_size(array)
-
+def using_size( array )
+  return array.size
 end
